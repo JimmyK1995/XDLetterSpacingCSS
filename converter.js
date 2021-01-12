450ba0a4cb00083a3d44c0140bb866aaf2e48e5c
Jimmy Kaltoft <Kaltoft1995@live.dk>
2021-01-12 12:19:20 +0000

    Project is done, mobile, tablet, and desktop responsive. Styling has been added as well as functionality.

--- converter.js
+++ converter.js
@@ -2,11 +2,24 @@ let fontSize = document.querySelector(".fontSizeValue");
 let letterSpacing = document.querySelector(".letterSpacingValue");
 let submit = document.querySelector(".button1");
 
+function fontSizeLength(){
+    return fontSize.value.length;
+}
+
+function letterSpacingLength(){
+    return letterSpacing.value.length;
+}
+
+function addResult(){
+    
+    var result = document.querySelector(".result");
+    let thousand = 1000;
+    result.innerHTML = fontSize.value * letterSpacing.value / thousand;
+    fontSize.value = "";
+    letterSpacing.value = "";
+    console.log(result)
+}
 
 
+submit.addEventListener("click", addResult)
 
-submit.addEventListener("click", function(){
-
-
-
-})
